5bdf715578078f0a78e08e68ac5770d0f2aca496
Amber Oglesby <aoglesby5795@gmail.com>
2023-01-05 00:42:11 -0500

    update

--- main.js
+++ main.js
@@ -1,10 +1,10 @@
 //clock
-const $clockDisplay = document.querySelectorAll(".clock-display"); 
-const $clockprogressbar = document.querySelectorAll(".clock-progress-bar");
-/*const $clockface = document.querySelectorAll(".clock-face");
-const $clock = document.querySelectorAll(".clock"); */
+const $clockDisplay = document.querySelector(".clock-display"); //null
+const $clockprogressbar = document.querySelector(".clock-progress-bar"); //null +2overloads?
+const $clockface = document.querySelector(".clock-face"); //parent node
+const $clock = document.querySelector(".clock"); 
 
-const $inner = select.querySelectorAll('.outer .inner');
+const $inner = select.querySelectorAll(".outer .inner");
 
 
 function currentTime() {
@@ -15,31 +15,35 @@ function currentTime() {
     let ss = now.getSeconds();
 
     hh == (hh < 10) ? "0" + hh : hh;
-    mm = (mm < 10) ? "0" + mm : mm;
-    ss = (ss < 10) ? "0" + ss : ss;
+    mm == (mm < 10) ? "0" + mm : mm;
+    ss == (ss < 10) ? "0" + ss : ss;
 
-    var timeclock = hh + ":" + mm + ":" + ss + " ";  // this will affect $timeclock, look at name 
+    var clockDisplay = hh + ":" + mm + ":" + ss + " ";  // declared but never read 
 
    //variable names will target specific elements ex)timeclock
    // query requires $ 
 
 
 
-    var timeclock = setTimeout(function(){ currentTime() }, 1000);
+    var clockDisplay = setTimeout(function(){ currentTime() }, 1000); //undefined,void
 }
 
-currentTime();
+currentTime(); //reads as void
 
 //clock
+/*  For the clock i was attempting to use the querySelector to link this JS to HTML but was unsuccessful.the 
+the clock display has not changed and gives an undefined ue to the value never being read, the function for currentTime r
+reads as 'void'. I also am having a hard time understanding how to use (inner/outer HTML) */
 
 //timer bar
+//why does document.getElementByClass() not work for linking to html info?
 
 function clockprogress() {
     var clockProgressBar = document.querySelector(".clock-progress-bar") ;
-    var clockDisplay = document.querySelector(".clock-display") ;
+    var clockFace = document.querySelector(".clock-face") ;
     var startTimer = setInterval(progressBar,10) ;
     function progressBar(){
-        if(clockProgressBar.clientWidth < clockDisplay.clientWidth){
+        if(clockFace.clientWidth < clockProgressBar.clientWidth){
             clockProgressBar.style.width = clockProgressBar.clientWidth + 1 + "px";
         }
     else{
@@ -49,4 +53,29 @@ function clockprogress() {
     
     }
 }
+//time bar
+/* For the progres bar i was attempting to set the progress bar to change width every time the width 
+of the clock face is less than the progress bar, which is constant as the clock face has no width, I
+attempted to create a function, however i have not successfully gotten my JS to link with the HTML or CSS */
 
+//color change
+
+var i=0;
+var col = new Array("ff99cc","ff3399","b30059","800040","33001a");
+function change()
+{
+    document.body.style.backgroundColor=col[i];
+    i++
+    if(i>col.length)
+{
+    i=0;
+  }
+  
+  window.setTimeout("change()",1000);
+}
+ window.onload=change();
+
+ /* var generator = function() {
+    newColor = "#" +
+    (Math.random()*0xffffff<<0).toString(16)
+ }   ask more about this method */
